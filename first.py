a7114f941ea8a8ed4c8cc96e1eceec69c58955b3
GOVIND <mayurirathod619@gmail.com>
2019-11-21 15:17:31 +0530

    good sanket

--- first.py
+++ first.py
@@ -4,5 +4,5 @@ sub3=int(input("Enter the marks for sub3"))
 marks=(sub1+sub2+sub3)/3
 
 if marks >=40:
-    print("passed with percentage:",marks)
+    print(" student passed with percentage:",marks)
     
