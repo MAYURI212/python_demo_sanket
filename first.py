6897dc7fcc98b80eb760455c0dd37c5da879448e
sanket366 <sanketrmore.more19@gmail.com>
2019-11-21 13:48:49 +0530

    commit no 1

---
+++ first.py
@@ -0,0 +1,7 @@
+sub1=int(input("Enter the marks for sub1"))
+sub2=int(input("Enter the marks for sub2"))
+sub3=int(input("Enter the marks for sub3"))
+marks=(sub1+sub2+sub3)/3
+
+if marks >=40:
+    print("passed with percentage:",marks
